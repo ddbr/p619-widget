0001fbb039e970d75a4b40166ab910071f434c78
ddbr <daniel.braeker@gmail.com>
2016-06-01 21:44:28 +0200

    text

--- p619-image-widget.php
+++ p619-image-widget.php
@@ -44,7 +44,7 @@ class P619_image_widget extends WP_Widget {
 	 * @param array $instance Saved values from database.
 	 */
 	public function widget( $args, $instance ) {
-		/*echo $args['before_widget'];
+		echo $args['before_widget'];
 		if ( ! empty( $instance['title'] ) ) {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
@@ -52,7 +52,8 @@ class P619_image_widget extends WP_Widget {
 			echo $args['before_text'] . apply_filters( 'widget_text', $instance['text'] ) . $args['after_text'];
 		}
 		echo __( esc_attr( 'Hello, World!' ), 'text_domain' );
-		echo $args['after_widget'];*/
+		echo $args['after_widget'];
+
 
 		echo "text";
 
