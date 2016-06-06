a4c28ac5764da89c2126e9ac5cbec10fc8f2750e
ddbr <daniel.braeker@gmail.com>
2016-06-06 20:32:27 +0200

    rename files

--- p619-image-widget.php
+++ p619-image-widget.php
@@ -26,7 +26,7 @@ class P619_image_widget extends WP_Widget {
 	}
 
 	public function register_plugin_styles() {
-		wp_register_style( 'P619_image_widget', plugins_url( 'p619-widget/p619_image_widget.css' ) );
+		wp_register_style( 'P619_image_widget', plugins_url( 'p619-widget/p619-image-widget.css' ) );
 		wp_enqueue_style( 'P619_image_widget' );
 	}
 
