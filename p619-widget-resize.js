0392e4b07f838825e16c886539f224dae83d8461
ddbr <daniel.braeker@gmail.com>
2016-06-07 23:55:51 +0200

    syntax

--- p619-widget-resize.js
+++ p619-widget-resize.js
@@ -8,14 +8,14 @@ jQuery( document ).ready( function( $ ) {
   else if(window.addEventListener) {
       window.addEventListener('resize', function() {
           console.log('addEventListener - resize');
-          
+
           var size_h1 = 10;
           var size_p = 3;
 
           $(".widget_p619_text_widget").each(function() {
             var w = $( this ).width;
-            $( this ).find( h1 ).css("font-size", fwidth / 100 + size_h1);
-            $( this ).find( p ).css("font-size", fwidth / 100 + size_p);
+            $( this ).find( "h1" ).css("font-size", fwidth / 100 + size_h1);
+            $( this ).find( "p" ).css("font-size", fwidth / 100 + size_p);
           });
       }, true);
   }
